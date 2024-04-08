d9413e66c8d8a6eab95df384732530fb861d558e
nyoungno Ko <yghjbm@gmail.com>
2024-04-09 03:11:34 +0900

    secondcommit

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 //랜덤번호가 > 유전번호 up!!
 //Rest버튼을 누르면 게임이 리셋된다.
 // 5번의 기회를 다쓰면 게임이 끝난다. (더이상 추측 불가. 버튼이 disavle)
-//유전가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깍지 않는다.
+//유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깍지 않는다.
 //유저가 이미 입력한 숫자를 또입 입력하면, 알려준다. 기회를 깍지 않는다.
 
 
@@ -13,9 +13,17 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
 
-playButton.addEventListener("click",play);
-
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function () {
+  userInput.value=""
+});
 
 function pickRandomNum() {
     computerNum = Math.floor(Math.random() * 100) + 1;
@@ -25,14 +33,49 @@ function pickRandomNum() {
 
 function play() {
     let userValue = userInput.value;
+
+    if(userValue<1 || userValue>100) {
+      resultArea.textContent = "1과 100사이 숫자를 입력해 주세요";
+      return;
+    }
+
+    if(history.includes(userValue)){
+      resultArea.textContent = "이미 입력한 숫자입니다 다른 숫자를 입력해 주세요";
+      return;
+    }
+
+    chances --;
+    chanceArea.textContent = `남은기회:${chances}번`;
+    console.log("chance",chances);
+
+
     if (userValue < computerNum) {
       resultArea.textContent = "UP!!";
     } else if (userValue > computerNum) {
       resultArea.textContent = "DOWN!!"; 
     } else {
       resultArea.textContent = "맞추셨습니다!!";
+      gameOver = true
     }
 
+    history.push(userValue);
+    console.log(history);
+
+    if(chances < 1) {
+      gameOver = true;
+    }
+    if(gameOver == true) {
+      playButton.disabled = true
+    }
+}
+
+function reset() {
+  // user input창이 깨끗하게 정리하고
+  userInput.value = "";
+  // 새로운 번호가 생성되고
+  pickRandomNum();
+
+  resultArea.textContent = "결과값이 여기 나옵니다!";
 }
 pickRandomNum();
 
